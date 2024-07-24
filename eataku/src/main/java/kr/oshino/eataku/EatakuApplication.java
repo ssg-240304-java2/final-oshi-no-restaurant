7ca6bebf9e652bb748fe346b7358a27a58de246e
SANG HYEON <162261368+sanghyeonJwa@users.noreply.github.com>
2024-07-24 12:09:36 +0900

    프로젝트 기본 구조 생성

---
+++ eataku/src/main/java/kr/oshino/eataku/EatakuApplication.java
@@ -0,0 +1,13 @@
+package kr.oshino.eataku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatakuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatakuApplication.class, args);
+    }
+
+}
---
+++ eataku/src/test/java/kr/oshino/eataku/EatakuApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.oshino.eataku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatakuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
