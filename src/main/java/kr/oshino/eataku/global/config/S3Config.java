50b6d7b6d4a966ef9a711bf3a1955a6bada4c07f
SANG HYEON <162261368+sanghyeonJwa@users.noreply.github.com>
2024-08-02 10:26:22 +0900

    feat/fileUpload : 이미지 파일 업로드 구현, S3 설정
    이미지 파일 업로드 구현, S3 설정
    
    - 해당 Util 클래스에 front 단에서 받은 file을 method에 넣으면 저희
    UUID 명칭 변경하고 파일 서버에 저장 후, 해당 URL 반환합니다.

---
+++ src/main/java/kr/oshino/eataku/global/config/S3Config.java
@@ -0,0 +1,39 @@
+package kr.oshino.eataku.global.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Value("${cloud.aws.s3.endpoint}")
+    private String endPoint;
+
+    /**
+     * AWS S3에 연결할 수 있는 객체 생성 후 반환 메소드
+     * @return
+     */
+    @Bean
+    public AmazonS3 setS3Client() {
+        AmazonS3 s3 = AmazonS3ClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endPoint, region))
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+                .build();
+        return s3;
+    }
+}
