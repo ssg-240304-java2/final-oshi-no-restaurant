a51331304811ece3e59af66e41075d3291f49860
yoaruku <rhrudgh12@naver.com>
2024-08-06 18:55:06 +0900

    #24 feat: add fragments

---
+++ src/main/resources/static/js/reservation/reservationLoginEmail.js
@@ -0,0 +1,12 @@
+document.getElementById('userIdCheckBtn').addEventListener('click', function() {
+    this.classList.add('clicked');
+});
+document.getElementById('nicknameCheckBtn').addEventListener('click', function() {
+    this.classList.add('clicked');
+});
+document.getElementById('emailCheckBtn').addEventListener('click', function() {
+    this.classList.add('clicked');
+});
+document.getElementById('authCodeCheckBtn').addEventListener('click', function() {
+    this.classList.add('clicked');
+});
