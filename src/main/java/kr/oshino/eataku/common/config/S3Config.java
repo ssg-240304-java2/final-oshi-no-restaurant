b72224ad3ed93e556fae5b9f2fba203af90cf4fb
SANG HYEON <162261368+sanghyeonJwa@users.noreply.github.com>
2024-08-06 10:01:17 +0900

    refactor: 디렉토리 명칭 변경
    1. global 디렉토리 common 으로 명칭 변경
    2. pages 디렉토리 -> waiting 으로 명칭 변경

--- src/main/java/kr/oshino/eataku/common/config/S3Config.java
+++ src/main/java/kr/oshino/eataku/common/config/S3Config.java
@@ -1,4 +1,4 @@
-package kr.oshino.eataku.global.config;
+package kr.oshino.eataku.common.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
