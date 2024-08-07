12a3fff9755d9f495e4d625be7df7d1cd2889b7f
jisoo78 <ekelldks@naver.com>
2024-08-07 16:25:37 +0900

    #38 Feat : entity create
    리스트 리뷰 엔티티 생성

---
+++ src/main/java/kr/oshino/eataku/list/model/List_Info.java
@@ -0,0 +1,30 @@
+package kr.oshino.eataku.list.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "List_Info")
+@Data
+@Setter(AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class List_Info {
+
+    /* 리스트 정보 */
+    @Id
+    @Column(name = "list_info_no")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int listInfoNo;
+
+    /* 리스트 번호*/
+    @ManyToOne
+    @JoinColumn(name = "list_no")
+    private List list;
+
+    /* 매장 번호 */
+//    @ManyToOne
+//    @JoinColumn(name = "restaurant_no")
+//    private Restaurant restaurant;
+}
---
+++ src/main/java/kr/oshino/eataku/review/user/model/Scope.java
@@ -0,0 +1,6 @@
+package kr.oshino.eataku.review.user.model;
+
+public enum Scope {
+
+    STAR1, STAR2, STAR3, STAR4, STAR5
+}
