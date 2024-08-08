46fb2e10fc6e1ab6456f183a4cbaeaac0a8e7664
jisoo78 <ekelldks@naver.com>
2024-08-08 19:19:31 +0900

    #34 feat: list controller create
    리스트 컨트롤러 생성 및 시험

--- src/main/java/kr/oshino/eataku/list/controller/ListController.java
+++ src/main/java/kr/oshino/eataku/list/controller/ListController.java
@@ -1,4 +1,13 @@
 package kr.oshino.eataku.list.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ListController {
+
+    @GetMapping("/list")
+    public String list() {
+        return "list";
+    }
 }
