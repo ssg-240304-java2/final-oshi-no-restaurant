92a78029583250ecefca937800e0ff00e0cb87c5
SANG HYEON <162261368+sanghyeonJwa@users.noreply.github.com>
2024-08-20 00:13:13 +0900

    #49 Feat: 웨이팅 도메인 Exception 추가

--- src/main/java/kr/oshino/eataku/common/exception/info/WaitingExceptionInfo.java
+++ src/main/java/kr/oshino/eataku/common/exception/info/WaitingExceptionInfo.java
@@ -6,7 +6,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum WaitingExceptionInfo {
 
-    DUPLICATED_WAITING(HttpStatus.BAD_REQUEST, 1300, "이미 해당 매장에 대기중 입니다!");
+    DUPLICATED_WAITING(HttpStatus.BAD_REQUEST, 1300, "이미 해당 매장에 대기중 입니다!"),
+    REQUEST_ALREADY_HANDLED(HttpStatus.BAD_REQUEST, 1301, "이미 입장 처리된 웨이팅입니다."),
+    REQUEST_ALREADY_CANCELED(HttpStatus.BAD_REQUEST, 1302, "이미 취소된 웨이팅 정보입니다.");
 
     private final HttpStatus httpStatus;
     private final Integer code;
