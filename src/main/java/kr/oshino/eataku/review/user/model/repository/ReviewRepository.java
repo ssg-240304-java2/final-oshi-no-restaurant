fc044dc73508b7ebdfffb393d3fcca2f06907208
seoyoonjeong <dbswjd0318@gmail.com>
2024-08-21 09:02:35 +0900

    #169 feat: 상세페이지 구현

--- src/main/java/kr/oshino/eataku/review/user/model/repository/ReviewRepository.java
+++ src/main/java/kr/oshino/eataku/review/user/model/repository/ReviewRepository.java
@@ -1,8 +1,11 @@
 package kr.oshino.eataku.review.user.model.repository;
 
 import kr.oshino.eataku.member.entity.Member;
+import kr.oshino.eataku.restaurant.admin.entity.RestaurantInfo;
 import kr.oshino.eataku.review.user.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     int countByMember(Member toMember);
