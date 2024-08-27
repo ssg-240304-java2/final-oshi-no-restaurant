c9a33acece7911c139d9bca96b909a14093d4b07
Ko Kyung Ho <rhrudgh12@naver.com>
2024-08-27 19:39:24 +0900

    #243 feat: 이용내역 구현

--- src/main/resources/static/js/member/history.js
+++ src/main/resources/static/js/member/history.js
@@ -12,3 +12,15 @@ function filterData() {
 document.addEventListener('DOMContentLoaded', function() {
     filterData();  // Initialize with the default value
 });
+
+$('.btn-outline-danger').on('click', function () {
+
+    console.log($(this).data('restaurantno'));
+    console.log($(this).data('servicetype'));
+    console.log($(this).data('serviceno'));
+
+    const restaurantNo = $(this).data('restaurantno');
+    const serviceType = $(this).data('servicetype');
+    const serviceNo = $(this).data('serviceno');
+    window.location.href = '/reviewPage?restaurantNo='+restaurantNo+'&serviceType='+serviceType+'&serviceNo='+serviceNo;
+});
