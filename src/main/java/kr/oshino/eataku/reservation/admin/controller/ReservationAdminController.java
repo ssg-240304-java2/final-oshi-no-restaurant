883be42ed0bdb8cab4fb540a6a0ad12d7f02abe1
SANG HYEON <162261368+sanghyeonJwa@users.noreply.github.com>
2024-08-28 18:16:32 +0900

    feat: 관리자 페이지 예약 관리 기능 구현
    1. 예약 취소
    2. 예약 방문 처리

--- src/main/java/kr/oshino/eataku/reservation/admin/controller/ReservationAdminController.java
+++ src/main/java/kr/oshino/eataku/reservation/admin/controller/ReservationAdminController.java
@@ -4,20 +4,16 @@ import kr.oshino.eataku.member.model.dto.CustomMemberDetails;
 import kr.oshino.eataku.reservation.admin.model.dto.ReservationCountDTO;
 import kr.oshino.eataku.reservation.admin.model.dto.ReservationDTO;
 import kr.oshino.eataku.reservation.admin.service.ReservationAdminService;
-import kr.oshino.eataku.reservation.user.entity.Reservation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cglib.core.Local;
-import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Date;
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 
 @Controller
@@ -31,9 +27,61 @@ public class ReservationAdminController {
     // 예약 조회
     @GetMapping("/reservationCheck")
     public String getReservationsCount() {
-        return "restaurant/reservationCheck";
+        return "reservation/admin/reservationCheck";
     }
 
+
+    /**
+     * 날짜별 예약 정보 조회
+     * @param date
+     * @return
+     */
+    @GetMapping("/reservationInfo")
+    @ResponseBody
+    public ResponseEntity<List<ReservationDTO>> getReservationInfoByDate(@RequestParam("date") String date) {
+        CustomMemberDetails member = (CustomMemberDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Long loginedRestaurantNo = member.getRestaurantNo();
+
+        Date parseDate = Date.valueOf(date);
+        log.info("parseDate = {}", parseDate);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(reservationAdminService.getReservationInfos(parseDate, loginedRestaurantNo));
+    }
+
+
+    /**
+     * 예약 취소 (매장)
+     * @param reservationNo
+     * @return
+     */
+    @PatchMapping("/reservation/cancel/{reservationNo}")
+    @ResponseBody
+    public ResponseEntity<?> cancelReservation(@PathVariable Integer reservationNo) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(reservationAdminService.cancelReservation(reservationNo));
+    }
+
+
+    /**
+     * 예약 방문 처리 (매장)
+     * @param reservationNo
+     * @return
+     */
+    @PatchMapping("/reservation/enter/{reservationNo}")
+    @ResponseBody
+    public ResponseEntity<?> enterReservation(@PathVariable Integer reservationNo) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(reservationAdminService.enterReservation(reservationNo));
+    }
+
+
+
+    /**
+     * 캘린더에 표시할 예약 건수 조회
+     * @param date
+     * @return
+     */
     @PostMapping("/reservationCheck")
     @ResponseBody
     public List<ReservationCountDTO> getReservationInfo(@RequestParam(value = "date", defaultValue = "") String date) {
